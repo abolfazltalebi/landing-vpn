8e7e510de2966db904dcec23693904d418313652
Abolfazl <a2019@2019s-MacBook-Air.local>
2024-08-17 23:43:54 +0330

    changes

---
+++ src/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    rewind : true,
+  } );
+  
+  splide.mount();
+  
+splide.mount();
+new Splide(".splide", {
+  type: "loop",
+  autoplay: "pause",
+  perPage: 4,
+});
+
